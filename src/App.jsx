0a32906f6ad2d86edd1da9760b1290ef7237b55c
jimmymendy <jimmymendy@Pros-MacBook-Pro.local>
2022-08-15 17:11:20 +0200

    redux blog

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import PostList from "./features/posts/PostList";
+import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<PostList />} />
+
+        <Route path='post'>
+          <Route index element={<AddPostForm />} />
+          <Route path=':postId' element={<SinglePostPage />} />
+          <Route path='edit/:postId' element={<EditPostForm />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/features/posts/PostList.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsError,
+  getPostsStatus,
+  fetchPosts,
+} from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postsStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postsStatus, dispatch]);
+
+  // const orderedPosts = posts
+  //   .slice()
+  //   .sort((a, b) => b.date.localeCompare(a.date)); // Could we have simply just Unshift method in the post Slice ?
+
+  // const renderedPosts = orderedPosts.map((post) => (
+
+  // ));
+
+  let content;
+  if (postsStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postsStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postsStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
+  return <section>{content}</section>;
+};
+
+export default PostList;
